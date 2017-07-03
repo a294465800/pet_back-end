3ff58aff903a7148c7346cffcce8286e3871489e
SHINING <jack7770126@126.com>
2017-07-03 16:14:59 +0800

    [new function] finish the commodity list page.

--- public/js/admin_index_action.js
+++ public/js/admin_index_action.js
@@ -22,4 +22,57 @@ $(function () {
 		$(this).addClass('active')
 	})
 
+	/*
+	* 商品全选
+	* */
+	var $all_commodity = $('#all_commodity')
+	var checkbox = true
+	$all_commodity.on('click',function (e) {
+		$('.commodity-id').attr('checked', checkbox)
+		e.stopPropagation()
+		checkbox = !checkbox
+	})
+
+	//商品删除
+	var $commodityDel = $('#commodityDel')
+	$commodityDel.on('click',function () {
+		var $delCommodity = $('.admin-commodity-info-item input:checked')
+		if($delCommodity.length > 0){
+			if(confirm('确定删除这些商品吗？')){
+				$delCommodity.parents('.admin-commodity-info-item').remove()
+				alert('删除成功！')
+			}
+		}else {
+			alert('请先选择要删除的商品！')
+		}
+	})
+
+	//商品下架
+	var $commodityDown = $('#commodityDown')
+	$commodityDown.on('click',function () {
+		var $delCommodity = $('.admin-commodity-info-item input:checked').parents('.admin-commodity-info-item')
+		if($delCommodity.length > 0){
+			if(confirm('确定下架这些商品吗？')){
+				$delCommodity.find('.commodity_status').html('已下架')
+				alert('下架成功！')
+			}
+		}else {
+			alert('请先选择要下架的商品！')
+		}
+	})
+
+	//商品上架
+	var $commodityUp = $('#commodityUp')
+	$commodityUp.on('click',function () {
+		var $delCommodity = $('.admin-commodity-info-item input:checked').parents('.admin-commodity-info-item')
+		if($delCommodity.length > 0){
+			if(confirm('确定上架这些商品吗？')){
+				$delCommodity.find('.commodity_status').html('出售中')
+				alert('上架成功！')
+			}
+		}else {
+			alert('请先选择要上架的商品！')
+		}
+	})
+
 })
