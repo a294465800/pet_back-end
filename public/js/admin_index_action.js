76bf074a82f54f565667815c79ceb4ea2cc16f4d
SHINING <jack7770126@126.com>
2017-07-03 18:28:30 +0800

    [new function] now comodity add page can choose some category of comodity.

--- public/js/admin_index_action.js
+++ public/js/admin_index_action.js
@@ -6,7 +6,7 @@ $(function () {
 	var $admin_navigation_li = $('.admin-navigation-li')
 	var $admin_navigation_ul_ul = $('.admin-navigation-ul-ul')
 	$admin_navigation_li.on('click', function () {
-		if($(this).hasClass('active')){
+		if ($(this).hasClass('active')) {
 			return
 		}
 		$admin_navigation_li.removeClass('active')
@@ -23,11 +23,26 @@ $(function () {
 	})
 
 	/*
-	* 商品全选
-	* */
+	 * 商品类别选中
+	 * */
+	var $commodity_category = $('.admin-commodity-form .checkbox-inline')
+	$commodity_category.on('click', function (e) {
+		e.stopPropagation()
+		if ($(this).find('input:checked').length > 0) {
+			$(this).find('.glyphicon').removeClass('glyphicon-unchecked').addClass('glyphicon-check')
+			$(this).css('color', '#ff963d')
+		} else {
+			$(this).find('.glyphicon').removeClass('glyphicon-check').addClass('glyphicon-unchecked')
+			$(this).css('color', '#3d3d3d')
+		}
+	})
+
+	/*
+	 * 商品全选
+	 * */
 	var $all_commodity = $('#all_commodity')
 	var checkbox = true
-	$all_commodity.on('click',function (e) {
+	$all_commodity.on('click', function (e) {
 		$('.commodity-id').attr('checked', checkbox)
 		e.stopPropagation()
 		checkbox = !checkbox
@@ -35,42 +50,42 @@ $(function () {
 
 	//商品删除
 	var $commodityDel = $('#commodityDel')
-	$commodityDel.on('click',function () {
+	$commodityDel.on('click', function () {
 		var $delCommodity = $('.admin-commodity-info-item input:checked')
-		if($delCommodity.length > 0){
-			if(confirm('确定删除这些商品吗？')){
+		if ($delCommodity.length > 0) {
+			if (confirm('确定删除这些商品吗？')) {
 				$delCommodity.parents('.admin-commodity-info-item').remove()
 				alert('删除成功！')
 			}
-		}else {
+		} else {
 			alert('请先选择要删除的商品！')
 		}
 	})
 
 	//商品下架
 	var $commodityDown = $('#commodityDown')
-	$commodityDown.on('click',function () {
+	$commodityDown.on('click', function () {
 		var $delCommodity = $('.admin-commodity-info-item input:checked').parents('.admin-commodity-info-item')
-		if($delCommodity.length > 0){
-			if(confirm('确定下架这些商品吗？')){
+		if ($delCommodity.length > 0) {
+			if (confirm('确定下架这些商品吗？')) {
 				$delCommodity.find('.commodity_status').html('已下架')
 				alert('下架成功！')
 			}
-		}else {
+		} else {
 			alert('请先选择要下架的商品！')
 		}
 	})
 
 	//商品上架
 	var $commodityUp = $('#commodityUp')
-	$commodityUp.on('click',function () {
+	$commodityUp.on('click', function () {
 		var $delCommodity = $('.admin-commodity-info-item input:checked').parents('.admin-commodity-info-item')
-		if($delCommodity.length > 0){
-			if(confirm('确定上架这些商品吗？')){
+		if ($delCommodity.length > 0) {
+			if (confirm('确定上架这些商品吗？')) {
 				$delCommodity.find('.commodity_status').html('出售中')
 				alert('上架成功！')
 			}
-		}else {
+		} else {
 			alert('请先选择要上架的商品！')
 		}
 	})
